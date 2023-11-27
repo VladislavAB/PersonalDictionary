84cd47043dc6e502ff617a2d814eb632cdcee702
VladislavAB <vlad.byrdin@bk.ru>
2023-11-27 14:42:57 +0700

    refact: Refactored game

--- Game.py
+++ Game.py
@@ -2,6 +2,30 @@ import random
 import os
 import Dictionary
 
+def get_words_from_dict(count):
+    words = []
+    pd_words = list(pd.keys()) #Лист всех слов
+    while len(words) != count:
+        number = random.randint(0, len(pd)-1)
+        if pd_words[number] not in words:
+            words.append(pd_words[number])
+    return words
+# Возвращает список рандомных слов равное count.
+def get_answers(word):
+    answers = []
+    pd_definitions = []
+    answers.append(pd[word]['definition'])  # Add right answer
+
+    for word, description in pd.items():
+        pd_definitions.append(description["definition"])
+    while len(answers) != 4:
+        num = random.randint(0, len(pd)-1)
+        if pd_definitions[num] not in answers:
+            answers.append(pd_definitions[num])
+    random.shuffle(answers)
+    return answers
+# Возвращает список рандомных определений равное 4, включая правильное определение.
+
 pd = {}
 dict_path = 'dict.csv'
 dict_stat_path = 'stats.csv'
@@ -11,39 +35,12 @@ pd = Dictionary.pd_from_file(dict_path)
 
 count = int(input('Введите количество слов: '))
 
-pd_list_words = []
-pd_dict_definitions = []
-for key, value in pd.items():
-    pd_list_words.append(key)
-    pd_dict_definitions.append(value)
-
-pd_list_definitions = []
-for index in range(len(pd_dict_definitions)):
-    pd_list_definitions.append(pd_dict_definitions[index]['definition'])
-game_dict = {}
-words = []
-defenitions = []
-answers = []
-while len(words) != count//2:
-    number = random.randint(0, len(pd)-1)
-    if pd_list_words[number] not in words:
-        words.append(pd_list_words[number])
-        defenitions.append(pd_list_definitions[number])
-for index in range(0, len(words)):
-    game_dict[words[index]] = defenitions[index]
+words = get_words_from_dict(count)# Возвращает список рандомных слов равное count.
 
 for word in words:
-    number = random.randint(0, len(words)-1)
-    while len(answers) < 4:
-        answers.append(defenitions[words.index(word)])
-        answers.append(defenitions[number])
-    print(f'1.{answers[0]}, 2.{answers[1]}, 3.{answers[2]}, 4.{answers[3]}')
-    choice = int(input(f'Что означает слово {word}? Введите цифру: '))
-    if word ==
-#         в дикте сравнить
-    записать в answers
-
-
-
-print(game_dict)
-
+    answers = get_answers(word)# Возвращает список рандомных определений равное 4, включая правильное определение.
+    for index in range(len(answers)):
+        print(f'{index+1}.{answers[index]}')
+    guess = int(input(f'Что означает слово: {word}? '))
+    if pd[word]['definition'] == answers[int(guess)-1]:
+        print('Вы угадали!')
