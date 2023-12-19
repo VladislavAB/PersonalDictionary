84be5361139c718feada15d014dea0dde13fdc3b
VladislavAB <vlad.byrdin@bk.ru>
2023-12-19 12:48:23 +0700

    refact: Game_object refactored

--- Game_object.py
+++ Game_object.py
@@ -16,12 +16,16 @@ class Game:
         self.dictionary = Dictionary(self.dict_path, self.dict_stat_path)
         self.pd = self.dictionary.__pd_from_file__(self.dict_path)
         self.dict_stat = self.dictionary.read_dict_stat()
-        self.sorted_dict_stat = self.read_sorted_dict_stat()
 
         self.game_stat = self.read_game_stat()
-        self.sorted_game_stat = self.read_sorted_game_stat()
 
-    def read_sorted_dict_stat(self) -> list:
+        self.game_number = self.get_game_number()
+        self.pack = self.create_words()
+        self.wrong_answers = []
+        self.right_answers = []
+        self.current_game_stat = {}
+
+    def sort_dict_stat(self) -> list:
         dict_stats_list = []
         if os.path.exists(self.dict_stat_path):
             with open(self.dict_stat_path, 'r') as f:
@@ -50,7 +54,7 @@ class Game:
                     game_stats_dict[word] = {'wrong': int(wrong_answers), 'right': int(right_answers), 'last': last}
         return game_stats_dict
 
-    def read_sorted_game_stat(self) -> list:
+    def sort_game_stat(self) -> list:
         read_game_stat = self.game_stat
         sorted_game_statistic = []
         if read_game_stat:
@@ -62,12 +66,12 @@ class Game:
         else:
             return sorted_game_statistic
 
-    def game_number(self) -> int:
+    def get_game_number(self) -> int:
         game_stat_exist = True if os.path.exists(self.game_stat_path) else False
         game_number_ = None
         if len(self.pd.keys()) >= 10 and game_stat_exist is False:
             game_number_ = 1
-        elif len(self.sorted_game_stat) == 5:
+        elif len(self.game_stat) == 5:
             game_number_ = 2
         elif len(self.game_stat.keys()) >= 10:
             game_number_ = 3
@@ -95,12 +99,14 @@ class Game:
                 count -= 1
         return source
 
-    def create_words(self, game_number: int) -> list:
+    def create_words(self) -> list:
         pack = []
-        if game_number == 1:
+        game_stat = self.sort_game_stat()
+        dict_stat = self.sort_dict_stat()
+        if self.game_number == 1:
             pack = list(self.pd.keys())[:5]
             random.shuffle(pack)
-        elif game_number == 2:
+        elif self.game_number == 2:
             pack = list(self.pd.keys())[5:10]
             random.shuffle(pack)
         else:
@@ -108,9 +114,9 @@ class Game:
                 word = list(self.pd.keys())[random.randint(0, len(self.pd) - 1)]
                 if word not in pack:
                     pack.append(word)
-        unique_words_game = self.get_num_from_source(self.sorted_game_stat, pack, 2)
+        unique_words_game = self.get_num_from_source(game_stat, pack, 2)
         pack = self.change_elements(pack, unique_words_game)
-        unique_words_dict = self.get_num_from_source(self.sorted_dict_stat, pack, 1)
+        unique_words_dict = self.get_num_from_source(dict_stat, pack, 1)
         pack = self.change_elements(pack, unique_words_dict)
         return pack
 
@@ -142,11 +148,9 @@ class Game:
                 print('Вы ввели букву вместо цифры!')
         return guess
 
-    def play_game(self, pack: list):
-        wrong_answers = []
-        right_answers = []
+    def play(self) -> bool:
         question_number = 1
-        for word in pack:
+        for word in self.pack:
             answers = self.get_answers(word)
             print(f'Вопрос № {question_number}')
             for index in range(len(answers)):
@@ -155,65 +159,63 @@ class Game:
             print('-----------')
             guess = self.input_guess(word)
             if self.pd[word]['definition'] == answers[int(guess) - 1]:
-                right_answers.append(word)
+                self.right_answers.append(word)
                 print('Правильный ответ!')
             else:
-                wrong_answers.append(word)
+                self.wrong_answers.append(word)
                 print('Неправильный ответ!')
-        return right_answers, wrong_answers
+        self.current_game_stat = self.make_current_game_stat()
+        return True
 
-    def make_current_game_stat(self, right_answers: list, wrong_answers: list) -> dict:
+    def make_current_game_stat(self) -> dict:
         time = datetime.datetime.now()
         last = time.strftime('%d') + '|' + time.strftime('%b') + '|' + time.strftime('%y')
-        game_stat = {}
-        for word in right_answers:
-            game_stat[word] = {'right': 1, 'wrong': 0, 'last': last}
-        for word in wrong_answers:
-            game_stat[word] = {'wrong': 1, 'right': 0, 'last': last}
-        return game_stat
+        current_game_stat = {}
+        for word in self.right_answers:
+            current_game_stat[word] = {'right': 1, 'wrong': 0, 'last': last}
+        for word in self.wrong_answers:
+            current_game_stat[word] = {'wrong': 1, 'right': 0, 'last': last}
+        return current_game_stat
 
-    def update_dict_stats(self, current_game_stat: dict) -> dict:
+    def update_dict_stats(self) -> dict:
         time = datetime.datetime.now()
         last = time.strftime('%d') + '|' + time.strftime('%b') + '|' + time.strftime('%y')
-        for word in current_game_stat.keys():
+        for word in self.current_game_stat.keys():
             if word in self.game_stat.keys():
-                self.game_stat[word]['wrong'] = self.game_stat[word]['wrong'] + current_game_stat[word]['wrong']
-                self.game_stat[word]['right'] = self.game_stat[word]['right'] + current_game_stat[word]['right']
+                self.game_stat[word]['wrong'] = self.game_stat[word]['wrong'] + self.current_game_stat[word]['wrong']
+                self.game_stat[word]['right'] = self.game_stat[word]['right'] + self.current_game_stat[word]['right']
                 self.game_stat[word]['last'] = last
             else:
-                self.game_stat[word] = {'wrong': current_game_stat[word]['wrong'],
-                                        'right': current_game_stat[word]['right'],
-                                        'last': current_game_stat[word]['last']}
+                self.game_stat[word] = {'wrong': self.current_game_stat[word]['wrong'],
+                                        'right': self.current_game_stat[word]['right'],
+                                        'last': self.current_game_stat[word]['last']}
         return self.game_stat
 
-    def create_game_stats(self, current_game_stat: dict) -> True:
+    def create_game_stats(self) -> True:
         now = datetime.datetime.now()
         last = now.strftime('%d') + '|' + now.strftime('%b') + '|' + now.strftime('%y')
         with open(self.game_stat_path, 'w') as f:
             header = "Word^Wrong^Right^last_Date\n"
             f.write(header)
-            for word, stat in current_game_stat.items():
+            for word, stat in self.current_game_stat.items():
                 row = word + "^" + str(stat['wrong']) + "^" + str(stat['right']) + "^" + last + "\n"
                 f.write(row)
         return True
 
-    def save_game_stats(self, updated_stats: dict) -> True:
-        with open(self.game_stat_path, 'w') as f:
-            header = "Word^Wrong^Right^last_Date\n"
-            f.write(header)
-            for key, value in updated_stats.items():
-                row = key + "^" + str(value['wrong']) + "^" + str(value['right']) + "^" + value['last'] + "\n"
-                f.write(row)
+    def save_game_stats(self) -> True:
+        if self.game_stat_exist:
+            updated_stats = game.update_dict_stats()
+            with open(self.game_stat_path, 'w') as f:
+                header = "Word^Wrong^Right^last_Date\n"
+                f.write(header)
+                for key, value in updated_stats.items():
+                    row = key + "^" + str(value['wrong']) + "^" + str(value['right']) + "^" + value['last'] + "\n"
+                    f.write(row)
+        else:
+            self.create_game_stats()
         return True
 
 
 game = Game()
-game_number = game.game_number()
-pack = game.create_words(game_number)
-right_answers, wrong_answers = game.play_game(pack)
-current_game_stat = game.make_current_game_stat(right_answers, wrong_answers)
-if game.game_stat_exist:
-    updated_stats = game.update_dict_stats(current_game_stat)
-    game.save_game_stats(updated_stats)
-else:
-    game.create_game_stats(current_game_stat)
+game.play()
+game.save_game_stats()
